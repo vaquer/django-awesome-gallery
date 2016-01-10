a40d325c91836b51cc5f224ef972b974a0a1d12b
Francisco Vaquero <francisco@ip-172-31-62-14.ec2.internal>
2016-01-10 20:45:10 +0000

    improve architecture in files

--- build/lib.linux-x86_64-2.6/awesome_gallery/static/js/widgets/collections/set_items.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'underscore', 'backbone', 'gallery/widgets/models/item'], function ($, _, BB, Item){
-    return BB.Collection.extend({
-        model: Item,
-        initialize: function(){
-
-        }
-    });
-});
