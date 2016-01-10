f82eb2744e830476566aa47ffecbdaa757bdd11e
Francisco Vaquero <francisco@ip-172-31-62-14.ec2.internal>
2016-01-10 20:43:34 +0000

    improve architecture in files

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 import awesome_gallery
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 setup(
-    name='django_awesome_gallery',
+    name='django-awesome-gallery',
     version=".".join(map(str, awesome_gallery.__version__)),
     author='Francisco Vaquero',
     author_email='akura11.tt@gmail.com',
     intall_requires=[
-        'Django>=1.0',
+        'Django>=7.0',
         'boto',
         'dateutil',
         'python-mimeparse=>0.1.4',
@@ -20,6 +27,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
